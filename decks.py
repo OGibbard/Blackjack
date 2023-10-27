4fcea97aadcc9eb1a3255f53fc9ad61dff9a1232
OGibbard <gibbard.o@oundleschool.org.uk>
2023-10-27 02:03:39 +0100

    Created decks in module

---
+++ decks.py
@@ -0,0 +1,20 @@
+from itertools import product
+
+numbers=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+suits=['S','H','D','C']
+
+class card():
+    def __init__(self, num, suit):
+        self.number = num
+        self.suit = suit
+
+def classDeck():
+    deck=[]
+    for j in range(4):
+        for k in range(13):
+            deck.append(card(numbers[k],suits[j]))
+    return deck
+
+def tuplesDeck():
+    quickDeck=product(numbers, suits)
+    return quickDeck
